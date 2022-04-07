30369f772fd28951f516b8194cb2b45e6669bb4c
Tolulope Oyejide <emaytolex@gmail.com>
2022-04-07 14:16:41 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,9 @@
  *
  * Return: Always @ (Success)
  */
-int main (void)
+int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
 
