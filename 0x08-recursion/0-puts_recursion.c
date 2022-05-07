50f909c8f630ee53d160777adfba26aff4a5a90f
Tolulope Oyejide <emaytolex@gmail.com>
2022-05-07 08:09:04 -0700

    0-outs_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
-*_print_rev_recursion - main - check the code
-* @s: character of a string
-* Return: Always 0.
-*/
-void _print_rev_recursion(char *s)
+ * _puts_recursion - main - check the code
+ * @s: character of string
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
 {
-if (*s != '\0')
-{
-_print_rev_recursion(s + 1);
-_putchar(*s);
-}
+	if (*(s) != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*(s) == '\0')
+		_putchar('\n');
 }
