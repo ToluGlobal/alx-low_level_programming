72a6a0d0c17fee09384d21730e862c2ac7bb234f
Tolulope Oyejide <emaytolex@gmail.com>
2022-05-13 14:10:47 -0700

    main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,44 +1,32 @@
 #include "3-calc.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 /**
- * main - Prints result of an operation
- * @argc: Number of arguments
- * @argv: Value of arguments
- * Return: Returns an int.
+ * get_op_func - Compares function with pointer and returns the equal.
+ * @s: points a char
+ * Return: Returns the function and the operator when equal
 **/
 
-int main(int argc, char *argv[])
+int (*get_op_func(char *s))(int, int)
 {
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 
-if (argc != 4)
+int i = 0;
+
+while (ops[i].op != NULL)
 {
-printf("Error\n");
-exit(98);
+if (!strcmp(ops[i].op, s))
+return (ops[i].f);
+i++;
+}
+return (NULL);
 }
 
-if (
-(strcmp(argv[2], "+") &&
-strcmp(argv[2], "-") &&
-strcmp(argv[2], "*") &&
-strcmp(argv[2], "/") &&
-strcmp(argv[2], "%"))
-)
-{
-printf("Error\n");
-exit(99);
-}
-
-if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
-&& atoi(argv[3]) == 0)
-{
-printf("Error\n");
-exit(100);
-}
-
-printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
-
-return (0);
-}
