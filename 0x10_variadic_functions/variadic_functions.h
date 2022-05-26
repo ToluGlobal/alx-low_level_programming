cfef90cd14cbdffcaca7572769d755ab3681a97d
Tolulope Oyejide <emaytolex@gmail.com>
2022-05-26 13:27:54 -0700

    add them up all

--- 0x10_variadic_functions/variadic_functions.h
+++ 0x10_variadic_functions/variadic_functions.h
@@ -1,15 +1,14 @@
-#ifndef VariadicFunctions
-#define VariadicFunctions
+#ifndef VAR_FUNCTIONS_H
+#define VAR_FUNCTIONS_H
 
 #include <stdio.h>
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *seperator, const unsigned int n, ...);
-void print_strings(const char *seperator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-
-#endif /* VariadicFunctions */
+#endif /* VAR_FUNCTIONS_H */
 
 
