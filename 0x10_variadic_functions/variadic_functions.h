ddf12edefc6033e7b9bdb76a6ddeeddd87380a34
Tolulope Oyejide <emaytolex@gmail.com>
2022-05-26 09:20:06 -0700

    Variadic Functions

---
+++ 0x10_variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef VariadicFunctions
+#define VariadicFunctions
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+#endif /*VariadicFunctions*/
+root@e4eae7e71167:/alx-low_level_programming/0x10_variadic_functions#
+clear
+
+
