fb0bffe305962f42f4f8f24f64163b72c196e590
Tolulope Oyejide <emaytolex@gmail.com>
2022-06-03 19:23:31 -0700

    solved

---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_SING
+#define LISTS_SING
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct list_sing - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_sing
+{
+	char *str;
+	unsigned int len;
+	struct list_sing *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
+
